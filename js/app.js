debd2702cc3eb1c0a63c7f0db0edf9b78ea1c75a
OMKAR TIKONE <103162420+omkartikone@users.noreply.github.com>
2022-10-16 20:16:36 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+// for slide products
+
+
+$(document).ready(function () {
+    $('.food-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        autoplay:true,
+
+
+    });
+});
